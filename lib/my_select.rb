e100376b24337b5e20771d629ed1af4e91f441fe
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-04-15 00:23:02 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
-def my_select(collection)
- # your code here!
+def my_select(arr)
+  new = []
+  index = 0 
+  while index < arr.length 
+  if yield arr[index] == true
+    new << arr[index]
+  end 
 end
+new
+end
+nums = [1,2,3,4,5]
+my_select(nums) do |num|
+  if (num%2==0)
+    true 
+    else 
+      false 
+  end
+end
+      
+
