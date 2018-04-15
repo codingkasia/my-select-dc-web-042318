13f2114956ee9ed4e88ea5848569f282bef0f5e9
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-04-15 00:41:04 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(arr)
   new = []
   index = 0 
   while index < arr.length 
-  if (yield arr[index].even? == true)
+  if (yield arr[index].even?)
     new << arr[index]
   end 
   index +=1 
