79cb311b02e659767261a18e7aa20845384c45de
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-04-15 00:43:37 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
- # your code here!
+ 
+def my_select(arr)
+  new = []
+  index = 0 
+  while index < arr.length 
+  if (yield arr[index])
+    new << arr[index]
+  end 
+  index +=1 
+end
+new
 end
